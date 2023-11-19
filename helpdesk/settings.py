f193a85455fc5eae76e0ddfc39945e3fa378b46c
JseMatamoros <josematamoros916@gmail.com>
2023-11-19 15:58:41 -0300

    al parecer commit final?

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tz0nmj5r222hkvwa30uml2vjci75xg6g#aj@36qqqup+yz8%r='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
